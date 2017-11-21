fafa1e3090631f8a2301f106b1166eaaafa13560
rashmee <rashmee.prakash@gmail.com>
2017-11-20 22:07:03 -0800

    Remove characters from string!

---
+++ removeCharFromString.py
@@ -0,0 +1,8 @@
+# coding=utf-8
+# Remove character from a string
+
+theString = input("Enter sentence? ")
+theChar = input("Enter character to remove? ")
+theReplace = input("Enter character to replace? ")
+
+print theString.replace(theChar, theReplace)

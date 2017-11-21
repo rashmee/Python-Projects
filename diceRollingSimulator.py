051b8e9cdf53de53a3346a2f4e26c1d970fb609d
rashmee <rashmee.prakash@gmail.com>
2017-11-20 16:52:39 -0800

    Dice Rolling Project: Added total for dice and also added print statements for double 6's

--- diceRollingSimulator.py
+++ diceRollingSimulator.py
@@ -1,7 +1,18 @@
+#Program simulating the dice roll
+
 from random import randint
+
 repeat = True
+dice1 = randint(1,6)
+dice2 = randint(1,6)
+
 while repeat:
-    print("You rolled ", randint(1,6), randint(1,6))
+    print "You rolled ",dice1, "&",dice2
+    print "Your total is ",dice1+dice2
+    if(dice1==dice2):
+        print "DOUBLES! Great going!!!"
+    elif(dice1==6 & dice2==6):
+        print "DOUBLE SIXES!!! Way to go!"
     print("Do you want to roll again?")
     repeat = ("y" or "yes") in input().lower()
 

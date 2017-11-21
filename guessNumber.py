92b4b72ac0b8894717858dbde83b87410798db7e
rashmee <rashmee.prakash@gmail.com>
2017-11-20 17:17:11 -0800

    Guess the Number: print random number

---
+++ guessNumber.py
@@ -0,0 +1,17 @@
+# coding=utf-8
+# The Goal: Similar to the first project, this project also uses the random module in
+# Python. The program will first randomly generate a number unknown to the user.
+# The user needs to guess what that number is. In other words, the user needs to be
+# able to input information. If the user’s guess is wrong, the program should return
+# some sort of indication as to how wrong (e.g. The number is too high or too low).
+# If the user guesses correctly, a positive indication should appear. You’ll need
+# functions to check if the user input is an actual number, to see the difference
+# between the inputted number and the randomly generated numbers, and to then compare
+# the numbers.
+
+
+from random import randint
+
+_randomNum_ = randint(1, 100)
+
+print _randomNum_
